fe8bf595bbe97bd6df806b5ee547d6dd3751c619
Lukasz Kolodziejczyk <lukasz.kolodziejczyk@vp.pl>
2019-04-07 20:02:19 +0200

    Add map api

--- js/script.js
+++ js/script.js
@@ -9,10 +9,11 @@ var carouselOptions = {
 var slideTemplate = document.querySelector('#slide-template').innerHTML;
 Mustache.parse(slideTemplate);
 
-var slidesHtml = slideData.reduce(function(acc,slide,index) {
+var slidesHtml = '';
+slideData.forEach(function (slide, index) {
     slide.id = index;
-    return acc += Mustache.render(slideTemplate, slide)
-}, '');
+    slidesHtml += Mustache.render(slideTemplate, slide);
+});
 
 document.querySelector('.carousel').insertAdjacentHTML('afterbegin', slidesHtml);
 
@@ -21,9 +22,33 @@ var progressBar = document.querySelector('.progress-bar');
 carousel.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
+});
+var preventMapCenter = false;
+carousel.on('change', function(index){
+    if(preventMapCenter) {
+        preventMapCenter = false;
+    } else {
+        newCoords = slideData[index].coords;
+        map.panTo(newCoords);
+    }
 })
 
 var btnReset = document.querySelector('.my-btn');
 btnReset.addEventListener('click', function () {
     carousel.select(0, false, false);
-});
+})
+
+window.initMap = function () {
+
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 18, center: slideData[0].coords });
+
+    slideData.forEach(function (slide) {
+        new google.maps.Marker({
+            position: slide.coords,
+            map: map,
+            title: slide.title
+        });
+    })
+
+};
